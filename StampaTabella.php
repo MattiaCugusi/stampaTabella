5690d0e90720a456ba4e3e02bc98922f1cb20aa8
Mattia Cugusi <m.cugusi287gmail.com>
2024-10-05 18:33:22 +0200

    tabella completata

--- StampaTabella.php
+++ StampaTabella.php
@@ -8,16 +8,23 @@
 <body>
     <?php
       function stampaTabella($m, $n){
-         echo "<table>";
+         echo "<table style = 'border-collapse: collapse; border: 1px solid black'>";
           for ($i = 0; $i < $m; $i++){
             echo "<tr>";
              for ($j=0; $j < $n; $j++){
-                echo "<td>" . $i . "," . $j . "</td>";
+                echo "<td style= 'border: 1px solid black; padding: 10px 10px 10px 10px'>" . $i . "," . $j . "</td>";
              }
+             echo "</tr>";
           }
 
           echo "</table>";
       }
+
+      $righe = rand(1,10);
+      $colonne = rand(1,10);
+
+      stampaTabella($righe,$colonne);
+
     ?>
     
 </body>
